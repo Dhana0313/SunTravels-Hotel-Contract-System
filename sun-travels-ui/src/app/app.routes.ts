29099a54a77391c6fc11dee0aa56af15b2e5baba
Dhananjaya Jayaweera <dhananjayaj@codegen.net>
2026-04-22 22:00:58 +0530

    Define the routes,Update the App Component TypeScript and Build the Navigation Bar

--- sun-travels-ui/src/app/app.routes.ts
+++ sun-travels-ui/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ContractManagement } from './features/contract-management/contract-management';
+import { ContractViewer } from './features/contract-viewer/contract-viewer';
+import { SearchEngine } from './features/search-engine/search-engine';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'search', component: SearchEngine },
+  { path: 'add-contract', component: ContractManagement },
+  { path: 'view-contracts', component: ContractViewer },
+  { path: '', redirectTo: '/search', pathMatch: 'full' } // Default landing page
+];
